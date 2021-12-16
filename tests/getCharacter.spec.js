96d296cdf603feb819cec243fea6c1d77b420675
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-12-16 14:38:53 -0300

    Requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,48 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    //fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined()
+  });
+
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    })
+  });
+
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    })
+  });
+
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    })
+  });
+
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('arya').name).toEqual('Arya Stark');
+    expect(getCharacter('BrIeNne').name).toEqual('Brienne Tarth');
+    expect(getCharacter('meliSSanDre').name).toEqual('Melissandre');    
+  });
+
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('jon snow')).toBeUndefined()
   });
 });
